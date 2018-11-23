e1f3565240ef4a2afbc48cd8ab8958e9ef51c798
Ollzi <oloflid@gmail.com>
2018-11-23 09:34:22 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace TimeCounter
+{
+    partial class Timecounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Timecounter));
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgData = new System.Windows.Forms.DataGridView();
+            this.Hours = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Minutes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWorkDays = new System.Windows.Forms.TextBox();
+            this.lblRemaining = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtStartTime = new System.Windows.Forms.TextBox();
+            this.lblEndTime = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(24, 48);
+            this.txtHours.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(66, 31);
+            this.txtHours.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Timmar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Minuter";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(118, 48);
+            this.txtMinutes.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(66, 31);
+            this.txtMinutes.TabIndex = 2;
+            this.txtMinutes.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMinutes_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "-";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(200, 44);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 44);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Lägg till";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dgData
+            // 
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hours,
+            this.Minutes});
+            this.dgData.Location = new System.Drawing.Point(24, 100);
+            this.dgData.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dgData.Name = "dgData";
+            this.dgData.Size = new System.Drawing.Size(488, 423);
+            this.dgData.TabIndex = 6;
+            // 
+            // Hours
+            // 
+            this.Hours.DataPropertyName = "Hours";
+            this.Hours.HeaderText = "Hours";
+            this.Hours.Name = "Hours";
+            // 
+            // Minutes
+            // 
+            this.Minutes.DataPropertyName = "Minutes";
+            this.Minutes.HeaderText = "Minutes";
+            this.Minutes.Name = "Minutes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 540);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total: ";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(140, 544);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 25);
+            this.lblTotal.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Arbetsdagar";
+            // 
+            // txtWorkDays
+            // 
+            this.txtWorkDays.Location = new System.Drawing.Point(477, 45);
+            this.txtWorkDays.Name = "txtWorkDays";
+            this.txtWorkDays.Size = new System.Drawing.Size(22, 31);
+            this.txtWorkDays.TabIndex = 10;
+            this.txtWorkDays.Text = "5";
+            this.txtWorkDays.TextChanged += new System.EventHandler(this.txtWorkDays_TextChanged);
+            // 
+            // lblRemaining
+            // 
+            this.lblRemaining.AutoSize = true;
+            this.lblRemaining.Location = new System.Drawing.Point(24, 579);
+            this.lblRemaining.Name = "lblRemaining";
+            this.lblRemaining.Size = new System.Drawing.Size(122, 25);
+            this.lblRemaining.TabIndex = 11;
+            this.lblRemaining.Text = "Gå hem tid:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(556, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Starttid";
+            // 
+            // txtStartTime
+            // 
+            this.txtStartTime.Location = new System.Drawing.Point(561, 44);
+            this.txtStartTime.Name = "txtStartTime";
+            this.txtStartTime.Size = new System.Drawing.Size(75, 31);
+            this.txtStartTime.TabIndex = 13;
+            this.txtStartTime.Text = "07:00";
+            this.txtStartTime.TextChanged += new System.EventHandler(this.txtStartTime_TextChanged);
+            // 
+            // lblEndTime
+            // 
+            this.lblEndTime.AutoSize = true;
+            this.lblEndTime.Location = new System.Drawing.Point(153, 578);
+            this.lblEndTime.Name = "lblEndTime";
+            this.lblEndTime.Size = new System.Drawing.Size(0, 25);
+            this.lblEndTime.TabIndex = 14;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(521, 469);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 54);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Töm";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Timecounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 659);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblEndTime);
+            this.Controls.Add(this.txtStartTime);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblRemaining);
+            this.Controls.Add(this.txtWorkDays);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgData);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHours);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Timecounter";
+            this.Text = "Time counter";
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgData;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hours;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Minutes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWorkDays;
+        private System.Windows.Forms.Label lblRemaining;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtStartTime;
+        private System.Windows.Forms.Label lblEndTime;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace TimeCounter
+{
+    public partial class Timecounter : Form
+    {
+        private DataTable _table;
+
+        public Timecounter()
+        {
+            InitializeComponent();
+            _table = new DataTable();
+            _table.Columns.Add(new DataColumn("Hours", typeof(int)));
+            _table.Columns.Add(new DataColumn("Minutes", typeof(int)));
+            _table.Columns.Add(new DataColumn("Total", typeof(int)));
+
+            dgData.AutoGenerateColumns = false;
+            dgData.DataSource = _table;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddRow();
+        }
+
+        private void AddRow()
+        {
+            var hours = string.IsNullOrEmpty(txtHours.Text) ? 0 : Convert.ToInt32(txtHours.Text);
+            var minutes = string.IsNullOrEmpty(txtMinutes.Text) ? 0 : Convert.ToInt32(txtMinutes.Text);
+
+            var row = _table.NewRow();
+            row["Hours"] = hours;
+            row["Minutes"] = minutes;
+            row["Total"] = (60*hours) + minutes;
+
+            _table.Rows.InsertAt(row, 0);
+
+            UpdateTotal();
+
+            txtHours.Text = string.Empty;
+            txtMinutes.Text = string.Empty;
+            txtHours.Focus();
+        }
+
+        private void UpdateTotal()
+        {
+            double totalMinutes = 0;
+
+            foreach (DataRow row in _table.Rows)
+            {
+                totalMinutes = totalMinutes + Convert.ToDouble(row["Total"]);
+            }
+
+            var hours = Math.Floor((totalMinutes / 60));
+            var minutes = totalMinutes - (hours*60);
+
+
+            lblTotal.Text = string.Format("{0} hours, {1} minutes", hours, minutes);
+
+            try
+            {
+                var hoursWorked = new TimeSpan(0, Convert.ToInt32(hours), Convert.ToInt32(minutes), 0);
+                var startHour = Convert.ToInt32(txtStartTime.Text.Split(':')[0]);
+                var startMinute = Convert.ToInt32(txtStartTime.Text.Split(':')[1]);
+                var startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, startHour, startMinute, 0);
+                var workHours = Convert.ToInt32(txtWorkDays.Text) * 8;
+
+                var remaining = new TimeSpan(0, workHours, 0, 0) - hoursWorked;
+                var endTime = startTime.Add(remaining).AddMinutes(30);
+                lblEndTime.Text = hours > 8 && remaining.Hours < 8 ? endTime.ToShortTimeString() : "";
+            }
+            catch (Exception e)
+            {
+                lblEndTime.Text = e.Message;
+            }
+            
+
+        }
+
+        private void txtMinutes_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddRow();
+            }
+        }
+
+        private void txtWorkDays_TextChanged(object sender, EventArgs e)
+        {
+            UpdateTotal();
+        }
+
+        private void txtStartTime_TextChanged(object sender, EventArgs e)
+        {
+            UpdateTotal();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            _table.Rows.Clear();
+            UpdateTotal();
+        }
+    }
+}
